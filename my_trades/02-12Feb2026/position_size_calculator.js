9a8fccad7c95bd7c2c7aa730a29ee1054d902dd4
Aditya Hajare <aditya43@gmail.com>
2026-02-17 20:48:14 +0530

    GKSL - 12 February 2026 (IPO Setup)

--- my_trades/02-12Feb2026/position_size_calculator.js
+++ my_trades/02-12Feb2026/position_size_calculator.js
@@ -1,7 +1,7 @@
 // ===== INPUTS =====
 const accountSize = 100000;        // Set account size
-const buyTriggerPrice = 113;   // <-- Set Buy trigger price
-const sl = 110.9;                // <-- Set SL price
+const buyTriggerPrice = 113;   // <-- Set Buy trigger price. Put exact Pivot High price here.
+const sl = 110.9;                // <-- Set SL price. Put exact Pivot Low price here.
 
 const TICK_SIZE = 0.1;
 
